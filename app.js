35560ba0a1554911449b933676e9cf6f920bcb40
aadityanandan <aadityanandan03@gmail.com>
2024-08-21 12:38:59 +0530

    fixed addvm and addcontactinfo

--- app.js
+++ app.js
@@ -8,16 +8,20 @@ const port = 5000;
 app.use(express.static('public'));
 app.use(express.json()); // Middleware to parse JSON bodies
 
-// POST route to verify SMTP configuration
-app.post('/verify-email-server', (req, res) => {
-    sendMail.verifySMTP((error, success) => {
-        if (error) {
-            res.status(500).json({ message: 'Error verifying SMTP configuration', error });
-        } else {
-            res.status(200).json({ message: 'Server is ready to take our messages' });
-        }
-    });
+app.post('/submit-form-endpoint', (req, res) => {
+    console.log(req.body);  // Log the received data
+    res.send('Form submitted successfully!');
 });
+// POST route to verify SMTP configuration
+// app.post('/verify-email-server', (req, res) => {
+//     sendMail.verifySMTP((error, success) => {
+//         if (error) {
+//             res.status(500).json({ message: 'Error verifying SMTP configuration', error });
+//         } else {
+//             res.status(200).json({ message: 'Server is ready to take our messages' });
+//         }
+//     });
+// });
 
 // Start the server
 app.listen(port, () => {
